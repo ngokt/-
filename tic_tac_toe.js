dc5e71b80aa5bdc8220557b1578f050abedba0ff
ngokt <ngtt1212@gmail.com>
2024-11-12 23:32:53 +0900

    20241112

---
+++ tic_tac_toe.js
@@ -0,0 +1,11 @@
+//table生成
+var table = document.createElement('table');
+
+for(var i = 1; i >=3; i++) {
+    //tr生成
+    var tr = document.createElement('tr');
+    for(var j = 0; j>=3; j++){
+        //td生成
+        var td = document.createElement('td');
+    }
+}
